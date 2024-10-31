971bf337e12fdf5d52af2bb54da8a409a77ff654
Stevan Sehn <stevanlinse@gmail.com>
2024-10-31 17:39:20 +0000

    Initial Commit

---
+++ swift-ios-classify-image/swift_ios_classify_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ios_classify_imageApp.swift
+//  swift-ios-classify-image
+//
+//  Created by Stevan Sehn on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_ios_classify_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-ios-classify-imageTests/swift_ios_classify_imageTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ios_classify_imageTests.swift
+//  swift-ios-classify-imageTests
+//
+//  Created by Stevan Sehn on 31/10/24.
+//
+
+import Testing
+@testable import swift_ios_classify_image
+
+struct swift_ios_classify_imageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
